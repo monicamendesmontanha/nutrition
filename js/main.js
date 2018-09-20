ec7a0848c82456f2101fe3fc26c4e4e0a4b48821
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-20 14:45:27 +1000

    add patient to table

--- js/main.js
+++ js/main.js
@@ -41,7 +41,35 @@ for(var i = 0; i < patients.length ; i++) {
 var addButton = document.querySelector("#add-patient");
 addButton.addEventListener("click", function() {
   event.preventDefault();
-  console.log("Fui clicado!");
-})
+
+  var form = document.querySelector("#form-add");
+
+  var name = form.name.value;
+  var weight = form.weight.value;
+  var height = form.height.value;
+  var fat = form.fat.value;
+
+  var patientTr = document.createElement("tr");
+
+  var nameTd = document.createElement("td");
+  var weightTd = document.createElement("td");
+  var heightTd = document.createElement("td");
+  var fatTd = document.createElement("td");
+  var bmiTd = document.createElement("td");
+
+  nameTd.textContent = name;
+  weightTd.textContent = weight;
+  heightTd.textContent = height;
+  fatTd.textContent = fat;
+
+  patientTr.appendChild(nameTd);
+  patientTr.appendChild(weightTd);
+  patientTr.appendChild(heightTd);
+  patientTr.appendChild(fatTd);
+
+  var table = document.querySelector("#table-patients");
+
+  table.appendChild(patientTr);
+});
 
 
