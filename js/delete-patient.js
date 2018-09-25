690792ee1a286dc0ece57efbca0f71de491cdcd0
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-25 13:52:53 +1000

    add setTimeout on delete

--- js/delete-patient.js
+++ js/delete-patient.js
@@ -1,6 +1,12 @@
 const table = document.querySelector("#table-patients");
 table.addEventListener("dblclick", function (event) {
   if (event.target.tagName == 'TD') {
+
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function () {
+      event.target.parentNode.remove();
+    }, 500);
+
     event.target.parentNode.remove()
   }
 });
