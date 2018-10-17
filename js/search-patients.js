895ab6a2d3732978acff850531f11767af144894
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-10-18 10:59:24 +1100

    add connectivity with server

---
+++ js/search-patients.js
@@ -0,0 +1,14 @@
+var buttonAdd = document.querySelector("#search-patients");
+
+buttonAdd.addEventListener("click", function () {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+  xhr.addEventListener("load", function () {
+    console.log(xhr.responseText);
+  })
+
+  xhr.send();
+
+});
